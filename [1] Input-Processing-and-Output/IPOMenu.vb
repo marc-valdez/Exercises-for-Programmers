39c57fc8a46020bcaad357ccbcd121e52497dba4
Marc Valdez <127009880+marc-valdez@users.noreply.github.com>
2024-03-19 13:28:00 +0800

    Added IPOMenu

---
+++ [1] Input-Processing-and-Output/IPOMenu.vb
@@ -0,0 +1,17 @@
+﻿Public Class IPOMenu
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        NumberOfCharacters.Show()
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        PrintingQuotes.Show()
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        MadLib.Show()
+    End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        RetirementCalculator.Show()
+    End Sub
+End Class
