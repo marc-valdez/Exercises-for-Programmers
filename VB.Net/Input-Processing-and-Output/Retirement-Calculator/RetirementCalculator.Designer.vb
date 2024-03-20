9ee30e509ecc0f5921c9cdd0e6afacb7dd935b36
Marc Valdez <127009880+marc-valdez@users.noreply.github.com>
2024-03-20 19:14:03 +0800

    Migrated to .NET8 from .NETFramework 4.8.1

---
+++ VB.Net/Input-Processing-and-Output/Retirement-Calculator/RetirementCalculator.Designer.vb
@@ -0,0 +1,133 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class RetirementCalculator
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        TableLayoutPanel1 = New TableLayoutPanel()
+        Label1 = New Label()
+        Label2 = New Label()
+        NumericUpDown1 = New NumericUpDown()
+        NumericUpDown2 = New NumericUpDown()
+        Label3 = New Label()
+        TableLayoutPanel1.SuspendLayout()
+        CType(NumericUpDown1, ComponentModel.ISupportInitialize).BeginInit()
+        CType(NumericUpDown2, ComponentModel.ISupportInitialize).BeginInit()
+        SuspendLayout()
+        ' 
+        ' TableLayoutPanel1
+        ' 
+        TableLayoutPanel1.AutoSize = True
+        TableLayoutPanel1.AutoSizeMode = AutoSizeMode.GrowAndShrink
+        TableLayoutPanel1.ColumnCount = 2
+        TableLayoutPanel1.ColumnStyles.Add(New ColumnStyle(SizeType.Percent, 33.144474F))
+        TableLayoutPanel1.ColumnStyles.Add(New ColumnStyle(SizeType.Percent, 66.85552F))
+        TableLayoutPanel1.Controls.Add(Label1, 1, 0)
+        TableLayoutPanel1.Controls.Add(NumericUpDown1, 0, 0)
+        TableLayoutPanel1.Controls.Add(Label2, 1, 1)
+        TableLayoutPanel1.Controls.Add(NumericUpDown2, 0, 1)
+        TableLayoutPanel1.Location = New Point(14, 14)
+        TableLayoutPanel1.Margin = New Padding(4, 3, 4, 3)
+        TableLayoutPanel1.Name = "TableLayoutPanel1"
+        TableLayoutPanel1.RowCount = 2
+        TableLayoutPanel1.RowStyles.Add(New RowStyle(SizeType.Percent, 50F))
+        TableLayoutPanel1.RowStyles.Add(New RowStyle(SizeType.Percent, 50F))
+        TableLayoutPanel1.Size = New Size(356, 58)
+        TableLayoutPanel1.TabIndex = 0
+        ' 
+        ' Label1
+        ' 
+        Label1.Anchor = AnchorStyles.Left
+        Label1.AutoSize = True
+        Label1.Location = New Point(121, 7)
+        Label1.Margin = New Padding(4, 0, 4, 0)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(141, 15)
+        Label1.TabIndex = 0
+        Label1.Text = "What is your current age?"
+        ' 
+        ' Label2
+        ' 
+        Label2.Anchor = AnchorStyles.Left
+        Label2.AutoSize = True
+        Label2.Location = New Point(121, 36)
+        Label2.Margin = New Padding(4, 0, 4, 0)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(199, 15)
+        Label2.TabIndex = 1
+        Label2.Text = "At what age would you like to retire?"
+        ' 
+        ' NumericUpDown1
+        ' 
+        NumericUpDown1.Anchor = AnchorStyles.None
+        NumericUpDown1.Location = New Point(4, 3)
+        NumericUpDown1.Margin = New Padding(4, 3, 4, 3)
+        NumericUpDown1.Name = "NumericUpDown1"
+        NumericUpDown1.Size = New Size(109, 23)
+        NumericUpDown1.TabIndex = 2
+        ' 
+        ' NumericUpDown2
+        ' 
+        NumericUpDown2.Anchor = AnchorStyles.None
+        NumericUpDown2.Location = New Point(4, 32)
+        NumericUpDown2.Margin = New Padding(4, 3, 4, 3)
+        NumericUpDown2.Name = "NumericUpDown2"
+        NumericUpDown2.Size = New Size(109, 23)
+        NumericUpDown2.TabIndex = 3
+        ' 
+        ' Label3
+        ' 
+        Label3.AutoSize = True
+        Label3.Font = New Font("Segoe UI", 9F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        Label3.Location = New Point(14, 84)
+        Label3.Margin = New Padding(4, 0, 4, 0)
+        Label3.Name = "Label3"
+        Label3.Size = New Size(235, 30)
+        Label3.TabIndex = 1
+        Label3.Text = "You have {XX} years left until you can retire." & vbCrLf & "It's {20XX}, so you can retire in {20XX}."
+        Label3.TextAlign = ContentAlignment.BottomLeft
+        ' 
+        ' RetirementCalculator
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(387, 131)
+        Controls.Add(Label3)
+        Controls.Add(TableLayoutPanel1)
+        Margin = New Padding(4, 3, 4, 3)
+        Name = "RetirementCalculator"
+        Text = "Retirement Calculator"
+        TableLayoutPanel1.ResumeLayout(False)
+        TableLayoutPanel1.PerformLayout()
+        CType(NumericUpDown1, ComponentModel.ISupportInitialize).EndInit()
+        CType(NumericUpDown2, ComponentModel.ISupportInitialize).EndInit()
+        ResumeLayout(False)
+        PerformLayout()
+
+    End Sub
+
+    Friend WithEvents TableLayoutPanel1 As TableLayoutPanel
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents NumericUpDown1 As NumericUpDown
+    Friend WithEvents NumericUpDown2 As NumericUpDown
+    Friend WithEvents Label3 As Label
+End Class
---
+++ VB.Net/MainMenu.vb
@@ -0,0 +1,5 @@
+﻿Public Class MainMenu
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        IPOMenu.Show()
+    End Sub
+End Class
