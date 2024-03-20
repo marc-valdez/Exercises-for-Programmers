dad5a68aade0c3d36db6e2b8116f848dc8ff85f3
Marc Valdez <127009880+marc-valdez@users.noreply.github.com>
2024-03-20 17:06:50 +0800

    Create CompoundInterest.cs

---
+++ [2] Calculations/Determining-Compound-Interest/CompoundInterest.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using static CSharp._2__Calculations.DataValidation;
+
+namespace CSharp._2__Calculations.Determining_Compound_Interest
+{
+    internal class CompoundInterest
+    {
+        class Interest
+        {
+            private readonly dynamic principalAmount, annualRate, yearsInvested, timesCompounded, goal;
+
+            public Interest(string _method)
+            {
+                if (_method == "predict")
+                    principalAmount = ValidatedNumber("What is the principal amount? ");
+                else if (_method == "invest")
+                    goal = ValidatedNumber("What is your goal amount? ");
+
+                annualRate = ValidatedNumber("What is the rate? ") / 100;
+                yearsInvested = ValidatedNumber("What is the number of years? ");
+                timesCompounded = ValidatedNumber("What is the number of times the interest is compounded per year? ");
+
+                var incrementalFactor = 1 + (annualRate / timesCompounded);
+                var exponent = timesCompounded * yearsInvested;
+                var compound = Math.Pow(incrementalFactor, exponent);
+
+                if (_method == "predict")
+                    goal = principalAmount * compound;
+                else if (_method == "invest")
+                    principalAmount = goal / compound;
+                Display(_method);
+            }
+
+            public void Display(string _method)
+            {
+                if (_method == "predict")
+                    Console.WriteLine($"${principalAmount:F2} invested at {annualRate * 100}% for {yearsInvested} years" +
+                                      $" compounded {timesCompounded} times per year is ${goal:F2}.");
+                else if (_method == "invest")
+                    Console.WriteLine($"To get ${goal}, given the annual rate is {annualRate * 100}% for {yearsInvested} years" +
+                                      $" compounded {timesCompounded} times per year.");
+                    Console.WriteLine($"You need to invest a principal amount of ${principalAmount:F2}.");
+            }
+        }
+
+        static void Main()
+        {
+            Console.WriteLine("[1] I want to know how much money I'll get after my investment period.");
+            Console.WriteLine("[2] I want to know how much money I'll need to invest to reach a certain goal.");
+            var choice = ValidatedNumber("Pick an option: ", _min: 1, _max: 2);
+            _ = choice == 1 ? new Interest("predict") : new Interest("invest");
+        }
+    }
+}
