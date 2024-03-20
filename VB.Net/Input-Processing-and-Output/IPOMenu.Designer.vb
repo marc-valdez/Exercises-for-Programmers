7b82f667ebdd1432539100cf9c6ffc3a4230ab60
Marc Valdez <127009880+marc-valdez@users.noreply.github.com>
2024-03-21 07:15:29 +0800

    Update menu contents

--- VB.Net/Input-Processing-and-Output/IPOMenu.Designer.vb
+++ VB.Net/Input-Processing-and-Output/IPOMenu.Designer.vb
@@ -1,9 +1,9 @@
-﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
 Partial Class IPOMenu
     Inherits System.Windows.Forms.Form
 
     'Form overrides dispose to clean up the component list.
-    <System.Diagnostics.DebuggerNonUserCode()> _
+    <System.Diagnostics.DebuggerNonUserCode()>
     Protected Overrides Sub Dispose(ByVal disposing As Boolean)
         Try
             If disposing AndAlso components IsNot Nothing Then
@@ -20,62 +20,67 @@ Partial Class IPOMenu
     'NOTE: The following procedure is required by the Windows Form Designer
     'It can be modified using the Windows Form Designer.  
     'Do not modify it using the code editor.
-    <System.Diagnostics.DebuggerStepThrough()> _
+    <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
-        Me.Button1 = New System.Windows.Forms.Button()
-        Me.Button2 = New System.Windows.Forms.Button()
-        Me.Button3 = New System.Windows.Forms.Button()
-        Me.Button4 = New System.Windows.Forms.Button()
-        Me.SuspendLayout()
-        '
-        'Button1
-        '
-        Me.Button1.Location = New System.Drawing.Point(12, 12)
-        Me.Button1.Name = "Button1"
-        Me.Button1.Size = New System.Drawing.Size(261, 23)
-        Me.Button1.TabIndex = 1
-        Me.Button1.Text = "Counting the Number of Characters"
-        Me.Button1.UseVisualStyleBackColor = True
-        '
-        'Button2
-        '
-        Me.Button2.Location = New System.Drawing.Point(12, 41)
-        Me.Button2.Name = "Button2"
-        Me.Button2.Size = New System.Drawing.Size(261, 23)
-        Me.Button2.TabIndex = 2
-        Me.Button2.Text = "Printing Quotes"
-        Me.Button2.UseVisualStyleBackColor = True
-        '
-        'Button3
-        '
-        Me.Button3.Location = New System.Drawing.Point(12, 70)
-        Me.Button3.Name = "Button3"
-        Me.Button3.Size = New System.Drawing.Size(261, 23)
-        Me.Button3.TabIndex = 3
-        Me.Button3.Text = "Mad Lib"
-        Me.Button3.UseVisualStyleBackColor = True
-        '
-        'Button4
-        '
-        Me.Button4.Location = New System.Drawing.Point(12, 99)
-        Me.Button4.Name = "Button4"
-        Me.Button4.Size = New System.Drawing.Size(261, 23)
-        Me.Button4.TabIndex = 4
-        Me.Button4.Text = "Retirement Calculator"
-        Me.Button4.UseVisualStyleBackColor = True
-        '
-        'IPOMenu
-        '
-        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
-        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(288, 135)
-        Me.Controls.Add(Me.Button4)
-        Me.Controls.Add(Me.Button3)
-        Me.Controls.Add(Me.Button2)
-        Me.Controls.Add(Me.Button1)
-        Me.Name = "IPOMenu"
-        Me.Text = "IPO"
-        Me.ResumeLayout(False)
+        Button1 = New Button()
+        Button2 = New Button()
+        Button3 = New Button()
+        Button4 = New Button()
+        SuspendLayout()
+        ' 
+        ' Button1
+        ' 
+        Button1.Location = New Point(14, 14)
+        Button1.Margin = New Padding(4, 3, 4, 3)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(304, 27)
+        Button1.TabIndex = 1
+        Button1.Text = "Counting the Number of Characters"
+        Button1.UseVisualStyleBackColor = True
+        ' 
+        ' Button2
+        ' 
+        Button2.Location = New Point(14, 47)
+        Button2.Margin = New Padding(4, 3, 4, 3)
+        Button2.Name = "Button2"
+        Button2.Size = New Size(304, 27)
+        Button2.TabIndex = 2
+        Button2.Text = "Printing Quotes"
+        Button2.UseVisualStyleBackColor = True
+        ' 
+        ' Button3
+        ' 
+        Button3.Location = New Point(14, 81)
+        Button3.Margin = New Padding(4, 3, 4, 3)
+        Button3.Name = "Button3"
+        Button3.Size = New Size(304, 27)
+        Button3.TabIndex = 3
+        Button3.Text = "Mad Lib"
+        Button3.UseVisualStyleBackColor = True
+        ' 
+        ' Button4
+        ' 
+        Button4.Location = New Point(14, 114)
+        Button4.Margin = New Padding(4, 3, 4, 3)
+        Button4.Name = "Button4"
+        Button4.Size = New Size(304, 27)
+        Button4.TabIndex = 4
+        Button4.Text = "Retirement Calculator"
+        Button4.UseVisualStyleBackColor = True
+        ' 
+        ' IPOMenu
+        ' 
+        AutoScaleDimensions = New SizeF(7.0F, 15.0F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(336, 156)
+        Controls.Add(Button4)
+        Controls.Add(Button3)
+        Controls.Add(Button2)
+        Controls.Add(Button1)
+        Margin = New Padding(4, 3, 4, 3)
+        Name = "IPOMenu"
+        Text = "IPO"
+        ResumeLayout(False)
 
     End Sub
 
--- VB.Net/MainMenu.vb
+++ VB.Net/MainMenu.vb
@@ -2,4 +2,8 @@
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
         IPOMenu.Show()
     End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        CalculationsMenu.Show()
+    End Sub
 End Class
