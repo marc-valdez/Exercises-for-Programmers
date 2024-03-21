43ec35e723e11977fdfac9a0cb39ed89bba28fd0
Marc Valdez <127009880+marc-valdez@users.noreply.github.com>
2024-03-21 10:08:17 +0800

    Added Self-Checkout

---
+++ VB.Net/Calculations/Self-Checkout/SelfCheckout.Designer.vb
@@ -0,0 +1,102 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class SelfCheckout
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        ItemList = New DataGridView()
+        Price = New DataGridViewTextBoxColumn()
+        Quantity = New DataGridViewTextBoxColumn()
+        Button1 = New Button()
+        Button2 = New Button()
+        CType(ItemList, ComponentModel.ISupportInitialize).BeginInit()
+        SuspendLayout()
+        ' 
+        ' ItemList
+        ' 
+        ItemList.AllowUserToDeleteRows = False
+        ItemList.AllowUserToResizeColumns = False
+        ItemList.AllowUserToResizeRows = False
+        ItemList.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.DisableResizing
+        ItemList.Columns.AddRange(New DataGridViewColumn() {Price, Quantity})
+        ItemList.Cursor = Cursors.IBeam
+        ItemList.EditMode = DataGridViewEditMode.EditOnEnter
+        ItemList.Location = New Point(12, 12)
+        ItemList.MultiSelect = False
+        ItemList.Name = "ItemList"
+        ItemList.RowHeadersVisible = False
+        ItemList.RowHeadersWidthSizeMode = DataGridViewRowHeadersWidthSizeMode.DisableResizing
+        ItemList.RowTemplate.Resizable = DataGridViewTriState.False
+        ItemList.ScrollBars = ScrollBars.Vertical
+        ItemList.SelectionMode = DataGridViewSelectionMode.CellSelect
+        ItemList.Size = New Size(220, 125)
+        ItemList.TabIndex = 0
+        ' 
+        ' Price
+        ' 
+        Price.Frozen = True
+        Price.HeaderText = "Price"
+        Price.Name = "Price"
+        ' 
+        ' Quantity
+        ' 
+        Quantity.Frozen = True
+        Quantity.HeaderText = "Quantity"
+        Quantity.Name = "Quantity"
+        ' 
+        ' Button1
+        ' 
+        Button1.Location = New Point(12, 143)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(75, 23)
+        Button1.TabIndex = 1
+        Button1.Text = "Add Item"
+        Button1.UseVisualStyleBackColor = True
+        ' 
+        ' Button2
+        ' 
+        Button2.Location = New Point(158, 143)
+        Button2.Name = "Button2"
+        Button2.Size = New Size(75, 23)
+        Button2.TabIndex = 2
+        Button2.Text = "Checkout"
+        Button2.UseVisualStyleBackColor = True
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(7.0F, 15.0F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(245, 176)
+        Controls.Add(Button2)
+        Controls.Add(Button1)
+        Controls.Add(ItemList)
+        Name = "Form1"
+        Text = "Self-Checkout"
+        CType(ItemList, ComponentModel.ISupportInitialize).EndInit()
+        ResumeLayout(False)
+    End Sub
+
+    Friend WithEvents ItemList As DataGridView
+    Friend WithEvents Price As DataGridViewTextBoxColumn
+    Friend WithEvents Quantity As DataGridViewTextBoxColumn
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+End Class
---
+++ VB.Net/Calculations/Self-Checkout/SelfCheckout.vb
@@ -0,0 +1,38 @@
+﻿Public Class SelfCheckout
+
+    Private Const TAX = 0.055
+
+    Private Sub On_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        For Each column As DataGridViewColumn In ItemList.Columns
+            column.SortMode = DataGridViewColumnSortMode.NotSortable
+        Next
+        ItemList.RowCount = 3
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        ItemList.Rows.Add()
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Dim _total, _subtotal, _tax As Decimal
+        For Each row As DataGridViewRow In ItemList.Rows
+            Dim price As Decimal = 0.0
+            Dim quantity As Integer = 0
+
+            If Decimal.TryParse(row.Cells("Price").Value?.ToString(), price) AndAlso
+               Integer.TryParse(row.Cells("Quantity").Value?.ToString(), quantity) Then
+                _subtotal += price * quantity
+            End If
+        Next
+
+        _tax = _subtotal * TAX
+        _total = _subtotal + _tax
+
+        Dim MessageString As New Text.StringBuilder
+        MessageString.Append($"Subtotal: ${_subtotal:F2}" & vbCrLf)
+        MessageString.Append($"Tax: ${_tax:F2}" & vbCrLf)
+        MessageString.Append($"Total: ${_total:F2}")
+
+        MessageBox.Show(MessageString.ToString, "Basket", MessageBoxButtons.OK, MessageBoxIcon.Information)
+    End Sub
+End Class
