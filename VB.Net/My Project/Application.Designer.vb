8ebbdbe8d85d4420d64e86feef6b3bee6dfef1c8
Marc Valdez <127009880+marc-valdez@users.noreply.github.com>
2024-03-21 10:12:25 +0800

    Update Application.Designer.vb

--- VB.Net/My Project/Application.Designer.vb
+++ VB.Net/My Project/Application.Designer.vb
@@ -33,7 +33,7 @@ Namespace My
         
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()>  _
         Protected Overrides Sub OnCreateMainForm()
-            Me.MainForm = Global.VB.Net.MainMenu
+            Me.MainForm = Global.VB.Net.Form1
         End Sub
         
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()>  _
