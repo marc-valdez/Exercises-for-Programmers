f9dd05561ad1beef464e011b9390f84a5eac4842
Marc Valdez <127009880+marc-valdez@users.noreply.github.com>
2024-03-22 16:19:12 +0800

    Update PrintingQuotes.vb

--- VB.Net/Input-Processing-and-Output/Printing-Quotes/PrintingQuotes.vb
+++ VB.Net/Input-Processing-and-Output/Printing-Quotes/PrintingQuotes.vb
@@ -1,13 +1,7 @@
 ﻿Public Class PrintingQuotes
-    Private Sub RichTextBox1_TextChanged(sender As Object, e As EventArgs) Handles RichTextBox1.TextChanged
+    Private Sub UpdateText(sender As Object, e As EventArgs) Handles RichTextBox1.TextChanged, TextBox1.TextChanged
         Dim quote As String = RichTextBox1.Text
         Dim speaker As String = TextBox1.Text
-        Label3.Text = speaker + "says, """ + quote + """"
-    End Sub
-
-    Private Sub TextBox1_TextChanged(sender As Object, e As EventArgs) Handles TextBox1.TextChanged
-        Dim quote As String = RichTextBox1.Text
-        Dim speaker As String = TextBox1.Text
-        Label3.Text = speaker + "says, """ + quote + """"
+        Label3.Text = speaker + " says, """ + quote + """"
     End Sub
 End Class
