9a71b3981366240d3c30445bc98c04800fd1b31c
Marc Valdez <127009880+marc-valdez@users.noreply.github.com>
2024-03-22 10:04:26 +0800

    Create DataValidation.py
    Separated data validation processes to another .py file

---
+++ Python/DataValidation.py
@@ -0,0 +1,16 @@
+def ValidatedNumber(prompt, _min=None, _max=None, _sentinel=None):
+    while True:
+        if _sentinel is not None:
+            user_input = input(prompt[:-2] + f" (or type \'{_sentinel}\' to end): ")
+            if user_input.upper() == _sentinel:
+                return None
+        else:
+            user_input = input(prompt)
+        try:
+            number = float(user_input)
+            if (_min is None or _min <= number) and (_max is None or _max >= number):
+                return number
+            else:
+                print(f"Input out of range. [{_min}-{_max}]")
+        except ValueError:
+            print("! Error: Input is NaN")
