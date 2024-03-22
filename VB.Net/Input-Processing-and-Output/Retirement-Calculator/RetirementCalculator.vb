4ac0268b71c5754a93b6c5252e4ecc266ca5f224
Marc Valdez <127009880+marc-valdez@users.noreply.github.com>
2024-03-22 16:18:57 +0800

    Update RetirementCalculator.vb

--- VB.Net/Input-Processing-and-Output/Retirement-Calculator/RetirementCalculator.vb
+++ VB.Net/Input-Processing-and-Output/Retirement-Calculator/RetirementCalculator.vb
@@ -1,24 +1,14 @@
 ﻿Public Class RetirementCalculator
-    Private _currentAge As Short
-    Private _ageTarget As Short
+    Private Sub GetInfo(sender As Object, e As EventArgs) Handles NumericUpDown1.ValueChanged, NumericUpDown2.ValueChanged
+        Dim _currentAge As Byte = NumericUpDown1.Value
+        Dim _ageTarget As Byte = NumericUpDown2.Value
 
-    Public Sub UpdateText()
-        Dim ageDiff = _ageTarget - _currentAge
+        Dim ageDiff As Byte = _ageTarget - _currentAge
         If ageDiff <= 0 Then
             Label3.Text = "You can already retire."
         Else
             Label3.Text = $"You have {ageDiff} years left until you can retire." & vbCrLf &
                           $"It's {DateTime.Now.Year}, so you can retire in {DateTime.Now.Year + (ageDiff)}."
         End If
-    End Sub
-
-    Private Sub NumericUpDown1_ValueChanged(sender As Object, e As EventArgs) Handles NumericUpDown1.ValueChanged
-        _currentAge = NumericUpDown1.Value
-        UpdateText()
-    End Sub
-
-    Private Sub NumericUpDown2_ValueChanged(sender As Object, e As EventArgs) Handles NumericUpDown2.ValueChanged
-        _ageTarget = NumericUpDown2.Value
-        UpdateText()
     End Sub
 End Class
